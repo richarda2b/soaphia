13776c4592b024186924045e2a2e4d6a0c7a9182
richarda2b <richard.alvarez.b@gmail.com>
2016-05-29 16:18:03 +1000

    Add raw data

---
+++ soaphia-ws/src/main/java/datasource/DataBuilder.java
@@ -0,0 +1,28 @@
+package datasource;
+
+import java.util.Map;
+
+/**
+ * Created by richard on 5/28/16.
+ */
+public class DataBuilder {
+    private Map<String, String> profile;
+    private Map<String, String> movies;
+    private Map<String, String> tvShows;
+
+    private Map<String, String> loadProfile()
+    {
+
+        return null;
+    }
+
+    private Map<String, String> loadMovies()
+    {
+        return null;
+    }
+
+    private Map<String, String> loadTvShows()
+    {
+        return null;
+    }
+}
